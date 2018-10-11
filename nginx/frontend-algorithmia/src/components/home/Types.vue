402c271f9538184cf8b2c76c408d5df99d06b6db
Zakaria Fellah <zkrfellahgmail.com>
2018-10-11 17:07:35 +0100

    add pretty category rendering

--- nginx/frontend-algorithmia/src/components/home/Types.vue
+++ nginx/frontend-algorithmia/src/components/home/Types.vue
@@ -2,7 +2,51 @@
     <div class="col-md-12" >
             <div class="categories" >
               <ul class="grid grid-nav category-grid category-even" >
-                <Item v-for="(item, index) in items" :key="index" v-bind="item" />
+                <li class="media category col-md-3" >
+                    <router-link  class="algo-list-link" to="/cat/Text Analysis">
+                        <div class="media-left" ><img src="@/assets/images/categories/text-analysis.svg" class="media-object category-icon"
+                            ></div>
+                        <div class="media-body" >
+                            <h4 class="media-heading" >
+                                   Text Analysis
+                            </h4>
+                        <p >
+                            Make sense of unstructured text
+                        </p>
+                        </div>
+                    </router-link>
+                </li>
+                <li class="media category col-md-3" >
+                    <router-link  class="algo-list-link" to="/cat/Computer Vision">
+                        <div class="media-left" ><img src="@/assets/images/categories/computer-vision.svg" class="media-object category-icon"
+                            ></div>
+                        <div class="media-body" >
+                        <h4 class="media-heading" >Computer Vision</h4>
+                        <p >Identify objects in images</p>
+                        </div>
+                    </router-link>
+                </li>
+                <li class="media category col-md-3" >
+                    <router-link  class="algo-list-link" to="/cat/Deep Learning">
+                        <div class="media-left" ><img src="@/assets/images/categories/deep-learning.svg" class="media-object category-icon"
+                            ></div>
+                        <div class="media-body" >
+                        <h4 class="media-heading" >Deep Learning</h4>
+                        <p >Learn from your data</p>
+                        </div>
+
+                    </router-link>
+                </li>
+                <li class="media category col-md-3" >
+                    <router-link  class="algo-list-link" to="/cat/Machine Learning">
+                        <div class="media-left" ><img src="@/assets/images/categories/machine-learning.svg" class="media-object category-icon"
+                            ></div>
+                        <div class="media-body" >
+                        <h4 class="media-heading" >Machine Learning</h4>
+                        <p >Teach your app to teach itself</p>
+                        </div>
+                    </router-link>
+                </li>
               </ul>
             </div>
             <!--        Tags        -->
@@ -25,29 +69,7 @@ export default {
       return{
           tags:[
               "utilities", "microservices","web","Sentiment Analysis"
-          ],
-          items :[
-              {
-                  img:"text-analysis.svg",
-                  title:"Text Analysis",
-                  desc:"Make sense of unstructured text"
-              },
-                            {
-                  img:"@/assets/images/categories/machine-learning.svg",
-                  title:"Machine Learning",
-                  desc:"Teach your app to teach itself"
-              },
-                            {
-                  img:"@/assets/images/categories/computer-vision.svg",
-                  title:"Computer Vision",
-                  desc:"Identify objects in images"
-              },
-                            {
-                  img:"@/assets/images/categories/deep-learning.svg",
-                  title:"Deep Learning",
-                  desc:"Learn from your data"
-              }          
-            ]
+          ]
       }
   }
 
